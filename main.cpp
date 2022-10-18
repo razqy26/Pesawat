42a49579bb9d277796df240cfb7ebf3cbae5ff46
Rina75 <waoderina75@gmail.com>
2022-10-18 15:39:35 +0800

    rinaa

--- main.cpp
+++ main.cpp
@@ -185,7 +185,6 @@ void tailP(){
 	glPopMatrix();
 }
 
-
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -234,3 +233,4 @@ int main(int argc, char **argv)
 // itumi saaja
 // okelah
 //begitu 89
+//rinaa
